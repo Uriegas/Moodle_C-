6147a7eae3bc24923eb3503f66cff7268d89ac3b
Uriegas <1930526@upv.edu.mx>
2020-08-02 21:19:42 -0500

    Modified files

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include "../include/input.h"
 #include "../include/menu.h"
-#include "../include/pregunta_calculada.h"
-#include "../include/pregunta_multiple.h"
-#include "../include/pregunta_simple.h"
 using namespace std;
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<COMMENTS SECTION>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -61,5 +58,50 @@ int main() {
     cout << "Ingrese la formula que se usara para calcular el resultado correcto: " << endl;
     formula = teacher.read_formula(question.answer_formula);
     cout << formula << endl;*/
+
+    //Instatiate classes about the question elemnts
+    Question question(CALCULATED);
+    int score;
+    //<<<<<<<<<< HERE is all the process of user interface (input from teacher) >>>>>>>>>>>>>>>
+    //<<<<<<<<<< Question Section >>>>>>>>>>>>>>>
+    std::cout << "Ingrese el nombre de la pregunta" << "\n";
+    std::cin.ignore();
+    question.read_question_name();
+    std::cout << "Ingrese el texto de la pregunta" << "\n";
+    question.read_question_text();
+    std::cout << "Ingrese la puntuacion por defectuo de la pregunta" << "\n"
+                << "(Puntos de la pregunta) Comunmente este valor es de 1" << "\n";
+    std::cin >> score;
+    question.set_default_score(score);
+    question.set_general_feedback();
+
+    //<<<<<<<<<< Answer Section >>>>>>>>>>>>>>>
+    Answer answer;
+    //Read answer input
+    std::cout << "Ingrese la formula de la respuesta correcta" << "\n";
+    //read_formula returns a postfix notation queue tokenized
+    answer.read_formula();
+    std::cout << "Ingrese la tolerancia y el tipo de tolerancia" << "\n"
+                << "Tipos de tolerancia:" << "\n"
+                << "1." << "\t" << "Relativa" << "\n"
+                << "2." << "\t" << "Nominal" << "\n"
+                << "3." << "\t" << "Geometrica" << "\n";
+    answer.set_tolerance();
+    answer.set_decimal();
+    answer.set_specific_feedback();
+    question.set_answer(answer);
+
+    //<<<<<<<<<< Unit Section >>>>>>>>>>>>>>>
+    question.set_units();
+    //<<<<<<<<<< Multiple Attempts Section >>>>>>>>>>>>>>>
+    //This section is not included in the simple question format
+
+    //<<<<<<<<<< Landmark Section >>>>>>>>>>>>>>>
+    question.set_landmarks();
+
+    //<<<<<<<<<< Created/Last Change Section >>>>>>>>>>>>>>>
+    question.time();
+    std::cout << "Pregunta creada en " << question.created << "\n"
+              << "Pregunta modifica en " << question.last_modified << "\n";
     return 0;
 }
